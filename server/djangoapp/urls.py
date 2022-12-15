e9b472d397068b93ef456e58f9a8ae4be39ed56b
Mohsen <mo.j@tuta.io>
2022-12-15 18:45:37 +0330

    navbar

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
 
