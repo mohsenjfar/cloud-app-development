db70af18cd19095feba5a0306e6941edbdf5e033
Mohsen Jamalifar <mo.j@tuta.io>
2022-12-16 15:02:54 +0330

    user login, logout, check

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,13 +7,9 @@ app_name = 'djangoapp'
 urlpatterns = [
     path(route='about', view=views.about, name='about'),
     path(route='contact', view=views.contact, name='contact'),
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
