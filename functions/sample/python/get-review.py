0abb3a96a03cfa270aa4923c18d1c783494f0be7
mohsenjfar <59406034+mohsenjfar@users.noreply.github.com>
2022-12-21 14:17:47 +0000

    Update get-review.py

--- functions/sample/python/get-review.py
+++ functions/sample/python/get-review.py
@@ -1,16 +1,16 @@
 from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('{apikey}')
 
-client = CloudantV1.new_instance(
-    authenticator=authenticator
-)
-client.set_service_url('{url}')
+def main(params):
+    authenticator = IAMAuthenticator(params['apikey'])
+    
+    client = CloudantV1(authenticator=authenticator)
+    client.set_service_url(params['url'])
+    
+    response = client.post_all_docs(
+      db='dealerships',
+      include_docs=True
+    ).get_result()
 
-response = client.post_all_docs(
-  db='dealerships',
-  include_docs=True
-).get_result()
-
-print(response)
+    return response
