9a535c5cf94fdb33d5f0fbcf2b64ed454b025292
Mohsen Jamalifar <mo.j@tuta.io>
2022-12-26 19:47:06 +0330

    temp commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,17 +1,37 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
 
 
-# Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, **kwargs):    
+    print(kwargs)    
+    print(f"GET from {url} ")
+    try:  
+        # Call get method of requests library with URL and parameters        
+        response = requests.get(url, headers={'Content-Type': 'application/json'},                                    
+        params=kwargs)    
+    except:
+        # If any error occurs
+        print("Network exception occurred") 
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
-
-# Create a `post_request` to make HTTP POST requests
-# e.g., response = requests.post(url, params=kwargs, json=payload)
-
+def post_request(url, payload, **kwargs):    
+    print(kwargs)    
+    print(f"POST to {url} ")
+    try:  
+        # Call get method of requests library with URL and parameters        
+        response = requests.post(url, params=kwargs, json=payload)
+    except:
+        # If any error occurs
+        print("Network exception occurred") 
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
